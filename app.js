8571fc182dd4b3f5872abfc01189fe4095ab3b4b
Dmitry <mityourik@gmail.com>
2023-10-16 12:58:08 -0300

    check joi validation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-/* eslint-disable indent */
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
@@ -16,22 +16,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
-
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  if (err.joi) {
-      const errorMessage = err.joi.details[0].message;
-      res.status(400).send({
-          status: 'error',
-          message: errorMessage || 'Validation failed',
-      });
-  } else {
-      res.status(500).send({
-          status: 'error',
-          message: 'Internal Server Error',
-      });
-  }
-});
+app.use(errors());
 
 app.listen(PORT, () => {
   console.log(`На порте: ${PORT}`);
